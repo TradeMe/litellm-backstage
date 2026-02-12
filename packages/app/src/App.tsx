d55772585cd722e5f0c4f4dd7b9b8ebe785828fa
Andres Videla <andres.videla@trademe.co.nz>
2026-02-13 10:36:59 +1300

    Update package scope from @internal to @trademe
    - Rename plugin to @trademe/plugin-litellm for GitLab NPM publishing
    - Update example app to reference @trademe scope
    - Plugin builds and packages successfully

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -36,7 +36,7 @@ import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
-import { LitellmPage } from '@internal/plugin-litellm';
+import { LitellmPage } from '@trademe/plugin-litellm';
 import { microsoftAuthApiRef } from '@backstage/core-plugin-api';
 
 const app = createApp({
